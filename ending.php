684b289761f9d85f41dcb55859d73b5bed9311b6
mwils-bp <michaelpwilson96@gmail.com>
2014-05-02 11:52:31 +0100

    Implemented intval() and mysqli_real_escape_string() in ending.php for security reasons againist sql injections

--- ending.php
+++ ending.php
@@ -1,16 +1,16 @@
 <?php
 require_once("libraries/password_compatibility_library.php");
 $link = mysqli_connect("cpd-db", "cpd", "dkfj55.1", "cpd");
-$pool_ref = $_POST['pool_ref'];
-$lesson_id = (int)$_POST['lesson_id'];
+$pool_ref = mysqli_real_escape_string($link, $_POST['pool_ref']);
+$lesson_id = intval($_POST['lesson_id']);
 #$minutes = (int)$row[3];
 #$resa = mysqli_query($link, "SELECT pool_id FROM pool WHERE pool_ref = '{$pool_ref}'");
 #        $rw = mysqli_fetch_row($resa);
 #        $pool_id = (int)$rw[0];
 
 if($_POST['action'] == "changing"){
-$user = $_POST['user'];
- $first_pass = $_POST['first_pass'];
+$user = mysqli_real_escape_string($link, $_POST['user']);
+$first_pass = $_POST['first_pass'];
 $password_hash = password_hash($first_pass, PASSWORD_DEFAULT);
 $qq = mysqli_query($link, "update user set user_password = '{$password_hash}' where user_login = '{$user}'");
 }
@@ -26,8 +26,8 @@ $query = mysqli_query($link,"UPDATE instances SET ttl = (ttl + interval 30 minut
 }
 
 if($_POST['action'] == "gfive"){
-$lesson_type = $_POST['lesson_type'];
-$ttl = $_POST['ttl'];
+$lesson_type = mysqli_real_escape_string($link, $_POST['lesson_type']);
+$ttl = mysqli_real_escape_string($link, $_POST['ttl']);
 $anuv = "insert into instances (instance_name, instance_state, instance_type, lesson_id, ttl, pool_ref) values('Unassigned', 'Requested', '" . $lesson_type  . "', " . $lesson_id  . ", '" . $ttl . "', '" . $pool_ref . "')";
         // loop depending on the choosen amount of instances
         for($i=0;$i<5;$i++)
