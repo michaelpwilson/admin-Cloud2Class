5de7dc7140802b29e78392b8ac1c0220d1b0a134
Michael Wilson <michaelpwilson96@gmail.com>
2014-08-26 01:13:31 +0100

    Update ending.php

--- ending.php
+++ ending.php
@@ -1,41 +1,65 @@
 <?php
-require_once("libraries/password_compatibility_library.php");
-$link = mysqli_connect("cpd-db", "cpd", "dkfj55.1", "cpd");
-$pool_ref = mysqli_real_escape_string($link, $_POST['pool_ref']);
-$lesson_id = intval($_POST['lesson_id']);
-#$minutes = (int)$row[3];
-#$resa = mysqli_query($link, "SELECT pool_id FROM pool WHERE pool_ref = '{$pool_ref}'");
-#        $rw = mysqli_fetch_row($resa);
-#        $pool_id = (int)$rw[0];
 
-if($_POST['action'] == "changing"){
-$user = mysqli_real_escape_string($link, $_POST['user']);
-$first_pass = $_POST['first_pass'];
-$password_hash = password_hash($first_pass, PASSWORD_DEFAULT);
-$qq = mysqli_query($link, "update user set user_password = '{$password_hash}' where user_login = '{$user}'");
-}
+ # Connect to mysql database
+ $link = mysqli_connect("cpd-db", "cpd", "dkfj55.1", "cpd");
+ # Get 'pool_ref'
+ $pool_ref = mysqli_real_escape_string($link, $_POST['pool_ref']);
+ # Get the 'lesson_id'
+ $lesson_id = intval($_POST['lesson_id']);
 
-if($_POST['action'] == "end"){
-mysqli_query($link,"UPDATE instances SET ttl = NOW() WHERE lesson_id={$lesson_id}");
-mysqli_query($link,"UPDATE lesson SET duration=timestampdiff(MINUTE,lesson_start,now()) WHERE lesson_id={$lesson_id}");
-}
+ # If the AJAX POST has an action of 'changing'
+ if($_POST['action'] == "changing"){
+ 	
+  # Require password compatibility library
+  require_once("libraries/password_compatibility_library.php");
+  # Get the User
+  $user = mysqli_real_escape_string($link, $_POST['user']);
+  # Get the password the user used in the Change Password modal
+  $pass = $_POST['first_pass'];
+  # Use the password_hash function on to generate a hashed version of the password
+  $password = password_hash($pass, PASSWORD_DEFAULT);
+  # Update the user by the user with the new password
+  $qq = mysqli_query($link, "update user set user_password = '{$password}' where user_login = '{$user}'");
+  
+ }
 
-if($_POST['action'] == "give"){
-$query = mysqli_query($link,"UPDATE lesson SET duration = duration+30 WHERE lesson_id = {$lesson_id}");
-$query = mysqli_query($link,"UPDATE instances SET ttl = (ttl + interval 30 minute) WHERE lesson_id = {$lesson_id}");
-}
+ # If the AJAX POST has an action of 'end'
+ if($_POST['action'] == "end"){
+ 	
+  # Update the instance by the $lesson_id, set time to live to NOW()
+  mysqli_query($link,"UPDATE instances SET ttl = NOW() WHERE lesson_id={$lesson_id}");
+  # Update the lesson by the $lesson_id
+  mysqli_query($link,"UPDATE lesson SET duration=timestampdiff(MINUTE,lesson_start,now()) WHERE lesson_id={$lesson_id}");
 
-if($_POST['action'] == "gfive"){
-$lesson_type = mysqli_real_escape_string($link, $_POST['lesson_type']);
-$ttl = mysqli_real_escape_string($link, $_POST['ttl']);
-$anuv = "insert into instances (instance_name, instance_state, instance_type, lesson_id, ttl, pool_ref) values('Unassigned', 'Requested', '" . $lesson_type  . "', " . $lesson_id  . ", '" . $ttl . "', '" . $pool_ref . "')";
-        // loop depending on the choosen amount of instances
-        for($i=0;$i<5;$i++)
-        {
-            if (!mysqli_query($link,$anuv))
-            {
-             	die('Error: ' . mysqli_error($link));
-            }
-	}
-}
+ }
+ 
+ # If the AJAX POST has an action of 'give'
+ if($_POST['action'] == "give"){
+
+  # update lesson by the $lesson_id, add 30 to the lesson's duration
+  $query = mysqli_query($link,"UPDATE lesson SET duration = duration+30 WHERE lesson_id = {$lesson_id}");
+  # update instance by the $lesson_id, add 30 minutes to the time to live
+  $query = mysqli_query($link,"UPDATE instances SET ttl = (ttl + interval 30 minute) WHERE lesson_id = {$lesson_id}");
+
+ }
+ # If the AJAX POST has an action of 'give'
+ if($_POST['action'] == "gfive"){
+ 
+     # Get the lesson_type
+     $lesson_type = mysqli_real_escape_string($link, $_POST['lesson_type']);
+     # Get the time to live
+     $ttl = mysqli_real_escape_string($link, $_POST['ttl']);
+     # insert statement to be repeat
+     $give = "insert into instances (instance_name, instance_state, instance_type, lesson_id, ttl, pool_ref) values('Unassigned', 'Requested', '" . $lesson_type  . "', " . $lesson_id  . ", '" . $ttl . "', '" . $pool_ref . "')";
+     # loop depending on the choosen amount of instances
+     for($i=0;$i<5;$i++) {
+     	
+        if (!mysqli_query($link,$give)) {
+         die('Error: ' . mysqli_error($link));
+        }
+        
+     }
+     
+  }
+  
 ?>
