66f4e35b41730d3f2e6363bbc8d101d10c5971cd
Michael Wilson <michaelpwilson96@gmail.com>
2014-08-26 01:16:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php
-  // checking for minimum PHP version
+
+  # checking for minimum PHP version
   if (version_compare(PHP_VERSION, '5.3.7', '<')) {
+    
+   # Exit
    exit("Sorry, you need PHP 5.3.7 !");
+   
   } else if (version_compare(PHP_VERSION, '5.5.0', '<')) {
-   // (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
+   
+   # (this library adds the PHP 5.5 password hashing functions to older versions of PHP)
    require_once("libraries/password_compatibility_library.php");
+    
   }
-  // include the configs / constants for the database connection
+  
+  # include the configs / constants for the database connection
   require_once("config/db.php");
-  // load the login class
+  # load the login class
   require_once("classes/Login.php");
-  // create a login object. when this object is created, it will do all login/logout stuff automatically
+  # create a login object. when this object is created, it will do all login/logout stuff automatically
   $login = new Login();
 
-  // ... I am logged in?
+  # Check if the User is logged in
   if ($login->isUserLoggedIn() == true) {
-  // Yes, I am logged in.
+  
+  # Yes, I am logged in and the main view has been launched.
   require("views/controller.php");
-  // The main view has been launched.
-  } else {
-  // I'm not logged in. So I'll go to the sign in page...
-  require("views/not_logged_in.php");
-  }
 
+  } else {
+  
+  # I'm not logged in. So I'll go to the sign in page...
+  require("views/not_logged_in.php");
+  
+  }
